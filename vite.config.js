1ed6c7886582ab1dab11b816867239675bc3a497
NeilBhosale <neil.bhosle010@gmail.com>
2025-04-15 19:18:13 -0400

    Update Vite configuration to set base path and build options

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  host: true, // Listen on all network interfaces
-  port: 5173 // Or your preferred port
-});
+  base: '/todoapp/', 
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+})
